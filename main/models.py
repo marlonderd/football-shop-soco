3a342a780141b7c943ad2871e8d34f11e00d23ad
marlonderd <marlondbatara9899@gmail.com>
2025-09-24 03:41:15 +0700

    Tugas Individu 4: Authentication Implementation

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = [
         ('clothes', 'Clothes'),
         ('shoes', 'Shoes'),
